a685554f7118c18e8a0b0e9e55eaffa437e01cfd
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-10-24 18:15:26 +0600

    added login page design

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -5,6 +5,7 @@ const GlobalError = ({error , reset} : { error: Error & { digest?: string }, res
         <html>
       <body>
         <h2>Something went wrong!</h2>
+        <p>{error.message}</p>
         <button onClick={() => reset()}>Try again</button>
       </body>
     </html>
