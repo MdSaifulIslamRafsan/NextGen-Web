ec9c8c85864073172bb03af143b17391a6980a5c
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-10-25 00:56:14 +0600

    added show password functionality

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const RegisterPage = () => {
     interface StrengthState  {
@@ -12,6 +13,9 @@ const RegisterPage = () => {
     }
     const [password , setPassword] = useState("");
     const [strength, setStrength] = useState(0);
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [confirmShowPassword, setConfirmShowPassword] = useState<boolean>(false);
+
 
     const updatedStrength : StrengthState  = {
         uppercase: /[A-Z]/.test(password),
@@ -44,15 +48,12 @@ const RegisterPage = () => {
         if (updatedStrength.length) {
             strengthValue += 1;
         };
-    
-        updatedStrength.strengthValue = strengthValue;
-    
         setStrength(strengthValue)
     }
     const handlePasswordChange = (e:  React.ChangeEvent<HTMLInputElement>) => {
         const newPassword = e.target.value;
         setPassword(newPassword);
-        evaluatePasswordStrength(newPassword);
+        evaluatePasswordStrength();
       };
     return (
         <div
@@ -116,43 +117,29 @@ const RegisterPage = () => {
               <div className="relative flex items-center">
                 <input
                   name="password"
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   onChange={handlePasswordChange}
                   required
                   className="bg-transparent w-full text-sm text-gray-800 border-b border-gray-400 focus:border-gray-800 px-2 py-3 outline-none placeholder:text-gray-800"
                   placeholder="Enter password"
                 />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#333"
-                  stroke="#333"
-                  className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
-                  viewBox="0 0 128 128"
-                >
-                  <path
-                    d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
-                  />
-                </svg>
+               <button type="button" onClick={()=>setShowPassword(!showPassword)} className="w-[18px] h-[18px] absolute right-2 cursor-pointer">
+                    {showPassword ? <FaEyeSlash className="text-xl text-black" /> : <FaEye className="text-xl text-black" />
+                    }
+                </button>
               </div>
               <div className="relative flex items-center">
                 <input
                   name="confirmPassword"
-                  type="password"
+                  type={confirmShowPassword ? "text" : "password"}
                   required
                   className="bg-transparent w-full text-sm text-gray-800 border-b border-gray-400 focus:border-gray-800 px-2 py-3 outline-none placeholder:text-gray-800"
                   placeholder="Confirm password"
                 />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#333"
-                  stroke="#333"
-                  className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
-                  viewBox="0 0 128 128"
-                >
-                  <path
-                    d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
-                  />
-                </svg>
+                <button  type="button" onClick={()=>setConfirmShowPassword(!confirmShowPassword)} className="w-[18px] h-[18px] absolute right-2 cursor-pointer">
+                    {confirmShowPassword ? <FaEyeSlash className="text-xl text-black" /> : <FaEye className="text-xl text-black" />
+                    }
+                </button>
               </div>
             </div>
               <div className="mt-2 grid grid-cols-5 gap-5">
