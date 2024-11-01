d76206213d863e458de0bc19c70920e8f9d74c27
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-11-02 04:01:43 +0600

    added sendEmail function argument provide

--- src/app/register/api/route.ts
+++ src/app/register/api/route.ts
@@ -4,6 +4,7 @@ import {hashSync } from "bcrypt-ts";
 // import jwt from 'jsonwebtoken';
 import { NextRequest, NextResponse } from 'next/server';
 import User from './../../../models/userModel';
+import { SendEmail } from '@/Helper/Sendmail';
 
 export const POST = async (request: NextRequest) => {
     try {
@@ -19,6 +20,14 @@ export const POST = async (request: NextRequest) => {
         console.log("line 20",hashPassword)
         const newUser = new User({...userInfo, password: hashPassword})
         await newUser.save();
+
+       const sendEmail = await SendEmail(email , "verify-email");
+       if(sendEmail === "Email sent successfully"){
+        return NextResponse.json({
+            message: "User created successfully",
+            status: 200,
+          });
+       }
         
         return NextResponse.json({message: "user successfully register" , status: 200 })
     } catch (error: unknown) {
